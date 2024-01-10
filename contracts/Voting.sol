0d88451bc3c22e186f00b731d51a62997f707745
Tom Salon <tom.salon62@gmail.com>
2024-01-10 19:38:29 +0100

    feat(clean) : comments + doc

--- contracts/Voting.sol
+++ contracts/Voting.sol
@@ -2,31 +2,43 @@
 pragma solidity 0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/// @title Voting is a smart contract manage voting sessions.
+/// @author Quentin GILLOT & Tom SALON
+/// @notice You can use this contract to register voters, proposals and run a voting session. Some features are only accessible by the owner of the contract.
 contract Voting is Ownable{
-    
+
+    /// Event emitted when a voter is registered.
+    /// @param voterAddress Address of the registered voter.
     event VoterRegistered(address voterAddress);
+
+    /// Event emittedd when the status changes.
+    /// @param previousStatus Old status.
+    /// @param newStatus New status.
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
+
+    /// Event emitted when a proposal is registered.
+    /// @param proposalId proposal id.
     event ProposalRegistered(uint proposalId);
+
+    /// Event emitted when a vote is registered.
+    /// @param voter Voter address.
+    /// @param proposalId Proposal Id.
     event Voted (address voter, uint proposalId);
 
-    mapping(address=> bool) private _whitelist;
-    mapping(address=> bool) private hasVoted;
-    mapping(uint=> address) proposalsOwnership;
-    mapping(uint=> Proposal) idToProposal;
-    uint indexProposal;
-    uint maxCount;
-
+    /// Struct to represent a voter.
     struct Voter {
         bool isRegistered;
         bool hasVoted;
         uint votedProposalId;
     }
 
+    /// Struct to represent a proposal.
     struct Proposal {
         string description;
         uint voteCount;
     }
 
+    /// Enum for the different status of a voting session.
     enum WorkflowStatus {
         RegisteringVoters,
         ProposalsRegistrationStarted,
@@ -35,70 +47,121 @@ contract Voting is Ownable{
         VotingSessionEnded,
         VotesTallied
     }
+
+    // Mapping of whitelisted voters.
+    mapping(address => bool) private _whitelist;
+
+    // Mapping to track whether a voter has voted.
+    mapping(address => bool) private _hasVoted;
+
+    // Mapping to link proposal id to its owner's address.
+    mapping(uint => address) private _proposalsOwnership;
+
+    // Mapping to linj the proposal id to the proposal itself.
+    mapping(uint => Proposal) private _idToProposal;
+
+    // Index of the next proposal id during registration.
+    uint private _indexProposal;
+
+    /// Number of count for the winnning(s) proposals.
+    uint private _maxCount;
+
+    /// Array to store the winning proposals ids.
+    uint[] winningProposalIds;
+
+    /// Current status the session is in.
+    WorkflowStatus internal currentStep = WorkflowStatus.RegisteringVoters;
     
+    /// Modifier to check wheter the sender is whitelisted or not.
     modifier isWhitelisted () {
         require (_whitelist[msg.sender] == true, "Cannot perform action because you are not whitelisted.");
         _;
     }
 
-    uint[] winningProposalIds;
-    WorkflowStatus internal currentStep = WorkflowStatus.RegisteringVoters;
-
-    constructor() Ownable(msg.sender){ 
+    constructor() Ownable(msg.sender) { 
         _whitelist[msg.sender] = true;
     }
 
     receive() external payable {}
     fallback() external payable {}
 
+    /// Function to advance to the next step in the voting workflow. Restricted to the owner.
     function nextStep() public onlyOwner {
-        emit WorkflowStatusChange(currentStep, WorkflowStatus(uint(currentStep) + 1));
+
+        // Loop between all the status. Once the workflow comes to an end, the next step is to register voters again.
+        emit WorkflowStatusChange(currentStep, WorkflowStatus((uint(currentStep) + 1) % 5));
         currentStep = WorkflowStatus(uint(currentStep) + 1);
-        
-        // If last step, reset whitelist ... to be ready to start a new voting session.
-        // Maybe add a mapping (uint => mapping(proposition)) to store propositions by voting session id.
     }
 
+    /// Function to whitelist/register voters. Restricted to the owner.
     function registerVoters(address _address) public onlyOwner {
+
+        // Check if the status is right.
         require (currentStep == WorkflowStatus.RegisteringVoters,"Cannot register voters yet.");
+
+        // Whitelist the address.
         _whitelist[_address] = true;
+
         emit VoterRegistered(_address);
     }
 
-    function registerProposal(string memory _description) public isWhitelisted{
+    /// Function to register proposals. Restricted to the whitelised voters.
+    function registerProposal(string memory _description) public isWhitelisted {
+
+        // Check if the status is right.
         require (currentStep == WorkflowStatus.ProposalsRegistrationStarted,"Cannot register proposal yet.");
-        proposalsOwnership[indexProposal] = msg.sender;
-        idToProposal[indexProposal] = Proposal(_description,0);
-        indexProposal++;
-        emit ProposalRegistered(indexProposal);
+
+        // Link the proposal id to the address of the sender and to the proposal itself.
+        _proposalsOwnership[_indexProposal] = msg.sender;
+        _idToProposal[_indexProposal] = Proposal(_description,0);
+
+        // Update the _indexProposal uint so the next registered proposal has the right id.
+        _indexProposal++;
+
+
+        emit ProposalRegistered(_indexProposal);
     }
 
-    function registerVote(uint _proposalId) public isWhitelisted{
+    /// Function to register votes. Restricted to the whitelised voters.
+    /// @param _proposalId Proposal Id.
+    function registerVote(uint _proposalId) public isWhitelisted {
+
+        // Check if current status is right, if the proposal the user wants to vote for exist and if the sender hasn't voted already.
+        // Here, checking if the proposalId < _indexProposal is enough to see if the proposal exists since there is no deletion allowed.
         require (currentStep == WorkflowStatus.VotingSessionStarted,"Cannot register vote yet.");
-        require (_proposalId < indexProposal,"Cannot vote for undefined proposal.");
-        require (hasVoted[msg.sender] != true,"You already voted.");
-        uint voteCount = ++idToProposal[_proposalId].voteCount;
-        if (voteCount > maxCount) {
-            maxCount = voteCount;
+        require (_proposalId < _indexProposal,"Cannot vote for undefined proposal.");
+        require (_hasVoted[msg.sender] != true,"You already voted.");
+
+        // We keep track of the winning proposal(s) each time a vote is tallied.
+        uint voteCount = ++_idToProposal[_proposalId].voteCount;
+        if (voteCount > _maxCount) {
+            _maxCount = voteCount;
             winningProposalIds = [_proposalId];
-        } else if (voteCount > maxCount) {
+        } else if (voteCount > _maxCount) {
             winningProposalIds.push(_proposalId);
         }
-        hasVoted[msg.sender] = true;
+
+        // Register the vote for the sender. He won't be able to vote for an other proposal.
+        _hasVoted[msg.sender] = true;
+
         emit Voted(msg.sender, _proposalId);
     }
     
+    /// Function to get the winning proposals ids.
     function getWinningProposal() public view returns (Proposal[] memory) {
+
+        // Check if the current step is right.
         require (currentStep == WorkflowStatus.VotingSessionEnded,"Cannot get winner yet.");
         Proposal[] memory winningProposals = new Proposal[](winningProposalIds.length);
         
         for (uint i = 0 ; i < winningProposalIds.length ; i++) {
-            winningProposals[i] = idToProposal[winningProposalIds[i]];
+            winningProposals[i] = _idToProposal[winningProposalIds[i]];
         }
         
         return winningProposals;
     }
 
+    /// Getter for the session's current status.
     function getCurrentStep() public view returns (WorkflowStatus) {
         return currentStep;
     }
